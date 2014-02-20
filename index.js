f50d2e1913a1a20f50a4f8e510eb18387734791d
Toby Ho <airportyh@gmail.com>
2014-02-20 10:50:20 -0500

    Automatically rewritten for npm.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Module dependencies.
  */
 
-var toFunction = require('to-function');
+var toFunction = require('component-to-function');
 
 /**
  * Sum the given `arr` with callback `fn(val, i)`.
